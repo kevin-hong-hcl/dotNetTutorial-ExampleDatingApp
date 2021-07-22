75b7435422a49f7a281c9fd93dfd3070e1aa06bd
Kevin Hong <kevin.hong@hcl.com>
2021-07-22 13:48:33 -0400

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,7 @@ namespace API
             options.UseSqlite(this._config.GetConnectionString("DefaultConnection"));
          });
          services.AddControllers();
+         services.AddCors();
          services.AddSwaggerGen(c =>
          {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +54,9 @@ namespace API
          app.UseHttpsRedirection();
 
          app.UseRouting();
+
+         // USECORS MUST BE BETWEEN USEROUTING AND USEENDPOINTS, AND BEFORE USEAUTHORIZATION
+         app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
          app.UseAuthorization();
 
