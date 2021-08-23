393bc35371b5edfe3f9df1d338e47eb68419541e
Kevin Hong <kevin.hong@hcl.com>
2021-08-23 15:29:40 -0400

    end of section 15

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,98 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMessageRepository messageRepository;
+        private readonly IMapper mapper;
+
+        public MessagesController(
+            IUserRepository userRepository, 
+            IMessageRepository messageRepository,
+            IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.messageRepository = messageRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessDto)
+        {
+            var username = User.GetUsername();
+            if (username == createMessDto.RecipientUsername.ToLower()) return BadRequest("You canont send messages to yourself.");
+
+            var sender = await userRepository.GetUserByUsernameAsync(username);
+            var recipient = await userRepository.GetUserByUsernameAsync(createMessDto.RecipientUsername);
+            if (recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = username,
+                RecipientUsername = recipient.UserName,
+                Content = createMessDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to send message");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages);
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+
+            return Ok(await messageRepository.GetMessageThread(currentUsername, username));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var currentUsername = User.GetUsername();
+            var message = await messageRepository.GetMessage(id);
+
+            if (message.Sender.UserName != currentUsername && message.Recipient.UserName != currentUsername) 
+                return Unauthorized();
+
+            if (message.Sender.UserName == currentUsername) message.SenderDeleted = true;
+            if (message.Recipient.UserName == currentUsername) message.RecipientDeleted = true;
+
+            if (message.SenderDeleted && message.RecipientDeleted) messageRepository.DeleteMessage(message);
+
+            if (await messageRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the message");
+        }
+    }
+}
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from "@kolkov/ngx-gallery";
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,17 +15,28 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberDetailComponent implements OnInit {
 
+  @ViewChild("memberTabs", {static: true}) memberTabs: TabsetComponent;
   member?: Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
+  activeTab: TabDirective;
+  messages: Message[] = [];
 
   constructor(
     private memberService: MembersService,
+    private messageService: MessageService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe(data => {
+      this.member = data.member;
+    });
+    // this.loadMember();
+
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    })
 
     if (!!this.member) {
       this.galleryOptions = [
@@ -35,18 +49,19 @@ export class MemberDetailComponent implements OnInit {
           preview: false
         }
       ];
-      
     }
+
+    this.galleryImages = this.getImages();
   }
 
 
-  loadMember() {
-    this.memberService.getMember(this.route.snapshot.paramMap.get("username")!).subscribe(member => {
-      this.member = member;
-      this.galleryImages = this.getImages();
-    });
+  // loadMember() {
+  //   this.memberService.getMember(this.route.snapshot.paramMap.get("username")!).subscribe(member => {
+  //     this.member = member;
+  //     this.galleryImages = this.getImages();
+  //   });
     
-  }
+  // }
 
   getImages(): NgxGalleryImage[] {
     const imageUrls = [];
@@ -58,6 +73,23 @@ export class MemberDetailComponent implements OnInit {
       }); 
     }
     return imageUrls;
+  }
+
+  loadMessages() {
+    this.messageService.getMessageThread(this.member.username).subscribe(messages => {
+      this.messages = messages;
+    })
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === "Messages" && this.messages.length === 0) {
+      this.loadMessages();
+    }
   }
 
 }
